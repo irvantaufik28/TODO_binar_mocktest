2493eb6346b039808147285e91028b25bd70ed7b
irvan taufik <irvantaufik28@gmail.com>
2022-12-03 23:09:25 +0700

    login

---
+++ src/helper/generateToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+  const accessToken = jwt.sign(
+    user,
+    process.env.JWT_KEY_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_AGE,
+    },
+  );
+
+  return accessToken;
+};
+
+module.exports = { generateToken };
--- src/usecase/auth.js
+++ src/usecase/auth.js
@@ -1,20 +1,46 @@
 class AuthUsecase {
-  constructor(userRepo, bycrpt) {
+  constructor(userRepo, bycrpt, generateToken) {
     this.userRepo = userRepo;
     this.bycrpt = bycrpt;
+    this.generateToken = generateToken;
   }
 
   async register(userData) {
     let result = {
-      isSuccess: true,
-      statusCode: 201,
+      isSuccess: false,
+      statusCode: 404,
       reason: null,
       data: null,
     };
+    if (userData.pin !== userData.confirmPin) {
+        result.reason = 'pin and confirm Pin not match';
+        return result
+    }
+
+    userData.pin = this.bycrpt.hashSync(userData.pin, 10);
 
     const user = await this.userRepo.register(userData);
-    result.data = user;
+    const token = this.generateToken(user)
+
+    const resUser = await this.userRepo.getUserByPin(userData.pin);
+    const userValue = {
+        id: resUser.id,
+        token: token
+    }
+
+    result.data = userValue;
     return result;
+  }
+
+  async login(userData) {
+    let result = {
+        isSuccess: false,
+        statusCode: 404,
+        reason: null,
+        data: null,
+      };
+
+
   }
 }
 
