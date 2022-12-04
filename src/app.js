453ff2f6d3b1e156e23f2486debd3a4de0d44615
irvan taufik <irvantaufik28@gmail.com>
2022-12-04 15:56:25 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const serverError = require('./middlerware/serverError');
 const token = require('./helper/generateToken');
 
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
