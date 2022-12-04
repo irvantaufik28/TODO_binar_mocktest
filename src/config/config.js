6eaf9a42d4893a77e7320fab3842f9836332fca2
irvan taufik <irvantaufik28@gmail.com>
2022-12-04 15:09:07 +0700

    add heroku

--- src/config/config.js
+++ src/config/config.js
@@ -12,4 +12,20 @@ module.exports = {
       rejectUnauthorized: false,
     },
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME ?? '127.0.0.1',
+    port: process.env.DB_PORT ?? 5432,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      rejectUnauthorized: false,
+    },
+  },
 };
